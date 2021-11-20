46f0f3819efe0581686fe15d6b38a034c694028a
GiovanniMarte <74544140+GiovanniMarte@users.noreply.github.com>
2021-11-20 17:52:06 +0100

    Update arch-base.sh

--- arch-base.sh
+++ arch-base.sh
@@ -25,12 +25,6 @@ echo "::1       localhost" >>  /etc/hosts
 echo "127.0.1.1 arch.localdomain    arch" >>  /etc/hosts
 clear
 
-# Set root password
-printf "Introduce la contraseña de administrador: "
-read -s ROOTPASS
-echo "root:$ROOTPASS" | chpasswd
-clear
-
 # Basic packages installation
 pacman -S base-devel nano grub efibootmgr networkmanager xorg i3 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance dmenu alacritty firefox picom nitrogen thunar wget neofetch htop unzip alsa-utils noto-fonts-emoji
 
@@ -42,6 +36,12 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # Enable services
 systemctl enable NetworkManager
 systemctl enable lightdm
+clear
+
+# Set root password
+printf "Introduce la contraseña de administrador: "
+read -s ROOTPASS
+echo "root:$ROOTPASS" | chpasswd
 clear
 
 # Create new user
