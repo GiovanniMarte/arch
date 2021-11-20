f36e35a4c43a354f8ca82e782f7d0cfd5a2a140f
GiovanniMarte <74544140+GiovanniMarte@users.noreply.github.com>
2021-11-20 03:40:00 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Introduce el tamaño de la partición EFI: " EFI
-read -p "Introduce el tamaño de la partición SWAP: " SWAP
+read "Introduce el tamaño de la partición EFI: " EFI
+read "Introduce el tamaño de la partición SWAP: " SWAP
 
 echo "$EFI, $SWAP"
