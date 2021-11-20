2e29dbc97eb48b2006c426e8a1e24dcb95b61c08
GiovanniMarte <74544140+GiovanniMarte@users.noreply.github.com>
2021-11-20 04:41:31 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,13 +12,15 @@ mount /dev/sda1 /mnt/efi
 swapon /dev/sda2
 mount /dev/sda3 /mnt
 
-# Base linux setup and chroot
+# Base linux setup and generate fstab
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
 
 # Get base install script
+cd /mnt
 curl -O https://raw.githubusercontent.com/GiovanniMarte/arch/main/arch-base.sh
 chmod +x arch-base.sh
 
-echo -e "\e[1;32mPara comenzar la instalación ejecuta ./arch-base.sh\033[0m"
+echo "Para comenzar la instalación ejecuta ./arch-base.sh"
+
+arch-chroot /mnt
