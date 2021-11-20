913e0f968628a09b30542c58595896d0112f6667
GiovanniMarte <74544140+GiovanniMarte@users.noreply.github.com>
2021-11-20 04:12:16 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -7,4 +7,4 @@ read -p "Introduce el tamaño de la partición SWAP: " SWAP
 clear
 
 # Create partitions
-echo -e "g\nn\n\n\n$EFI\nt\n1\n1\nnn\n\n\n$SWAP\nt\n1\n1\nw\n" | fdisk /dev/sda
+echo -e "g\nn\n\n\n$EFI\nt\n1\n1\nn\n\n\n$SWAP\nt\n1\n19\nn\n\n\n\nw\n" | fdisk /dev/sda
