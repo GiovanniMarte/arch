9212f4c16c9ac0e742607db85d45412aae29ee1a
GiovanniMarte <74544140+GiovanniMarte@users.noreply.github.com>
2021-11-20 18:53:00 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -9,11 +9,13 @@ SWAP=$((${SWAP}/2000))"M"
 echo -e "g\nn\n\n\n+512M\nn\n\n\n+${SWAP}\nn\n\n\n\nw\n" | fdisk /dev/sda
 
 # Format partitions
+printf "Formatting partitions..."
 mkfs.fat -F 32 /dev/sda1
 mkswap /dev/sda2
 mkfs.ext4 /dev/sda3
 
 # Mount partitions
+printf "Mounting partitions..."
 mkdir /mnt/efi
 mount /dev/sda1 /mnt/efi
 swapon /dev/sda2
